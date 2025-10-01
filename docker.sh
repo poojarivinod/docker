f0cb9bad56910afca835d9fecc8fa825e5a6b9eb
poojarivinod <vinod07081@gmail.com>
2025-10-01 19:37:28 +0530

    kubectl

--- docker.sh
+++ docker.sh
@@ -17,7 +17,7 @@ systemctl enable docker
 usermod -aG docker ec2-user
 
 #kubectl installation
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.9/2025-09-19/bin/linux/arm64/kubectl.sha256
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.9/2025-09-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
 
